131239bab199d33bd0f9cc4c9707dd241189cf0e
darilyne <evangelistadarilyn@gmail.com>
2025-09-21 03:22:49 -1000

    Zip Lists

---
+++ ziplist.ts
@@ -0,0 +1,13 @@
+console.log('START');
+const numbers: number[] = [1, 2, 3];
+const letters: string[] = ['a', 'b', 'c'];
+
+function zipList(): (number | string)[] {
+  const out: (number | string)[] = [];
+  for (let i = 0; i < Math.min(numbers.length, letters.length); i++) {
+    out.push(numbers[i], letters[i]);
+  }
+  return out;
+}
+
+console.log(zipList()); // -> [1, 'a', 2, 'b', 3, 'c']
