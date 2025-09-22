492bd20322ddb1c25b667193cd1376d493884cb4
darilyne <evangelistadarilyn@gmail.com>
2025-09-21 22:40:03 -1000

    Update ziplist.ts

--- ziplist.ts
+++ ziplist.ts
@@ -1,7 +1,7 @@
-console.log("START");
 const numbers: number[] = [1, 2, 3];
-const letters: string[] = ["a", "b", "c"];
+const letters: string[] = ['a', 'b', 'c'];
 
+console.log('Test');
 function zipList(): (number | string)[] {
   const out: (number | string)[] = [];
   for (let i = 0; i < Math.min(numbers.length, letters.length); i++) {
@@ -10,4 +10,20 @@ function zipList(): (number | string)[] {
   return out;
 }
 
-console.log(zipList()); // -> [1, 'a', 2, 'b', 3, 'c']
+function zipListReduce(): (number | string)[] {
+  return numbers
+    .slice(0, Math.min(numbers.length, letters.length))
+    .reduce(
+      (
+        acc: (number | string)[],
+        _item: number, // type is fine to leave as number
+        i: number,
+      ) => {
+        acc.push(numbers[i], letters[i]);
+        return acc;
+      },
+      [] as (number | string)[],
+    );
+}
+console.log(zipList());
+console.log(zipListReduce());
